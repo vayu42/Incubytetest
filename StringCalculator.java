bfa3b9c7557b54ed807c3a0502d98d4d2dc48c60
Vayu42 <kamujulasrikar14@gmail.com>
2020-06-03 13:52:08 +0530

    Should Add code if String is greater than zero

---
+++ StringCalculator.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class StringCalculator
+{
+    private String str;
+    
+    public String getString() {  
+         return name;  
+        }
+        
+    public void setString(String str) {  
+         this.str = str;  
+        }
+        
+    public int Add(String str) throws Exception    {
+        int  a =0,b=0,sum=0;
+        if(str.length()==0)
+            System.out.println(0);
+        else{
+            
+        }
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    
+	}
+}
+
